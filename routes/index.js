4771fb9e3043108a7eacf5221fe8a3a6fa62cdfe
Roku013 <roku.mail@gmail.com>
2022-04-19 13:16:54 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 const express = require('express');
-const router = express.Router();
+const celebritiesRouter = express.Router();
+
+const Celebrity = require('./../models/celebrity');
 
 // Handle GET request for website root
-router.get('/', (req, res, next) => {
+
+celebritiesRouter.get('/', (req, res, next) => {
   res.render('index');
 });
 
-module.exports = router;
+celebritiesRouter.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render('celebrities/index', { celebrities });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebritiesRouter.get('/celebrities/create', (req, res, next) => {
+  res.render('celebrities/create');
+});
+
+celebritiesRouter.get('/celebrities/:id', (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      res.render('celebrities/show', { celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebritiesRouter.post('/celebrities', (req, res, next) => {
+  const { name, occupation, catchphrase } = req.body;
+  Celebrity.create({ name, occupation, catchphrase })
+    .then((celebrity) => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      res.render('celebrities/create');
+    });
+});
+
+celebritiesRouter.post('/celebrities/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findByIdAndRemove(id)
+    .then(() => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = celebritiesRouter;
